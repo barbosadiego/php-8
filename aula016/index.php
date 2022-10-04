728fd13c825000117ddd739c0a8becaa178f70aa
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-04 11:02:51 -0300

    aula 16

---
+++ aula016/index.php
@@ -0,0 +1,10 @@
+<?php
+
+  //STRSTR
+  //Retorna o resto da string a partir de uma ocorrência
+
+  $a = 'Frase de teste no sistema.';
+  echo strstr($a, 'de'); //de teste no sistema.
+  echo strstr($a, 'n'); //no sistema.
+
+  
