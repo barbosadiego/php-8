3d035519a712293b95464527c7aba84152990fa0
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-04 15:21:29 -0300

    aula 29

---
+++ aula029/index.php
@@ -0,0 +1,15 @@
+<?php
+
+  //FILESYSTEM - CRIAR PASTAS NO PHP
+
+  //para criar uma pasta usamos a função mkdir
+  //mkdir('pasta1');
+
+  //para criar uma pasta dentro de outra
+  //mkdir('pasta1/pasta2');
+
+  //criar duas pastas, uma dentro da outra, ao mesmo tempo
+  //mkdir('teste/outra', 0777, true);
+
+  //remover uma pasta
+  rmdir('teste');
