7347e2385183428c996bfbd7d8133a55d18f5e9d
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-04 11:11:47 -0300

    aula 17

---
+++ aula017/index.php
@@ -0,0 +1,11 @@
+<?php
+
+  //STRPOS
+  // permite encontrar a posição (indice) da primeira 
+  // ocorrência de uma string dentro de outra string
+  // se não for encontrada a ocorrência, devolde false como resultado
+
+  $a = 'Frase de teste no sistema.';
+  $pos = strpos($a, 'se'); // 3
+  
+  
