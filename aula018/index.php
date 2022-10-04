097b824690eab1c48c54975c5af8d7cadf6ce505
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-04 11:17:21 -0300

    aula 18

---
+++ aula018/index.php
@@ -0,0 +1,11 @@
+<?php
+
+  //STRRPOS
+  /*
+    str r pos - procura a última ocorrência de uma string dentro da outra
+  */
+
+  $a = 'Frase de teste no sistema.';
+  $pos = strrpos($a, 'e'); // 22
+  
+  
