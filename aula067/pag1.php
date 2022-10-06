88c07be369aac7be99ea2b8bb2406a562e9b7e57
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-05 21:32:08 -0300

    aula 67

---
+++ aula067/pag1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php 
+      $nome = 'Joao';
+  ?>
+
+  <h1>Página 1</h1>
+  <a href="pag2.php?nome=<?= $nome ?>">Ir para a página 2</a>
+
+  <p><?= $nome ?></p>
+
+</body>
+</html>
