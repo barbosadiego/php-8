b031dc869deee19d583ed71381e04a3662afca8f
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-07 11:44:19 -0300

    aula 100

--- aula097/public/index.php
+++ aula097/public/index.php
@@ -1,27 +1,38 @@
 <?php
 
-  //CONECTAR PHPMYADMIN AO MYSQL
+require_once '../config.php';
 
-  //definição da ligação
-  $gestor = new PDO('mysql:host=localhost;dbname=loja;charset=utf8', 'root', '');
-  $dados = $gestor->query("SELECT * FROM clientes");
-  $clientes = $dados->fetchAll(PDO::FETCH_ASSOC);
+//CONECTAR PHPMYADMIN AO MYSQL
+
+//definição da ligação
+$gestor = new PDO(
+  'mysql:host=' . MYSQL_HOST . ';' .
+    'dbname=' . MYSQL_DATABASE . ';charset=utf8',
+  MYSQL_USER,
+  MYSQL_PASS
+);
+$dados = $gestor->query("SELECT * FROM clientes");
+$clientes = $dados->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <style>
-    table, th, td{
+    table,
+    th,
+    td {
       border: 1px solid black;
     }
   </style>
 </head>
+
 <body>
   <h3>CLIENTES</h3>
   <hr>
@@ -33,15 +44,16 @@
       </tr>
     </thead>
     <tbody>
-      <?php foreach($clientes as $cliente):?>
+      <?php foreach ($clientes as $cliente) : ?>
         <tr>
           <td><?= $cliente['id_cliente']; ?></td>
           <td><?= $cliente['nome']; ?></td>
         </tr>
-      <?php endforeach;?>
+      <?php endforeach; ?>
     </tbody>
   </table>
 
   <p>Total de clientes: <strong><?= count($clientes); ?></strong></p>
 </body>
+
 </html>
