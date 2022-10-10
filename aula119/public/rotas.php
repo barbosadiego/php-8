9e0f224c01e5aa08e6205e81008964bcbc00fc5f
barbosdiego <diego.gutemberg@hotmail.com>
2022-10-10 10:54:57 -0300

    aula 122

--- aula119/public/rotas.php
+++ aula119/public/rotas.php
@@ -10,6 +10,8 @@ if (!verifica_sessao() && $_SERVER["REQUEST_METHOD"] != 'POST') {
   $rota = 'login';
 } elseif (!verifica_sessao() && $_SERVER["REQUEST_METHOD"] == 'POST') {
   $rota = 'login_submit';
+} else {
+  $rota = 'logado';
 }
 
 //apresentar os layouts
@@ -27,13 +29,40 @@ switch ($rota) {
     $parametros = [
       ':usuario' => $_POST["usuario"]
     ];
-    $resultado = $bd->select("SELECT id_usuario, usuario FROM usuarios WHERE usuario = :usuario", $parametros);
-    print_r($resultado);
-    //concectar à base de dados
+    $resultado = $bd->select("SELECT id_usuario, usuario, senha FROM usuarios WHERE usuario = :usuario", $parametros);
+
+    if(count($resultado) == 0){
+      // die('erro');
+      $erro = "Login inválido 1";
+      require_once('../views/login_frm.php');
+      return;
+    }
+
+    $usuario = $resultado[0];
+
+    //verificar a senha
+    if(password_verify($_POST["senha"], $usuario['senha'])){
+      $_SESSION["usuario"] = $usuario['usuario'];
+      return;
+    } else {
+      // die('erro');
+      $erro = "Login inválido 2";
+      require_once('../views/login_frm.php');
+      return;
+    }
+
     break;
 
-  default:
+  case 'logado':
 
-    # code...
+    $bd = new EasyPDO();
+    $usuarios = $bd->select("
+      SELECT 
+      AES_DECRYPT(nome_completo, UNHEX(SHA2('". AES_KEY ."',512))) nome_completo
+      FROM usuarios
+    ");
+
+    print_r($usuarios);
+
     break;
 }
--- aula119/views/login_frm.php
+++ aula119/views/login_frm.php
@@ -11,6 +11,9 @@
           <label for="senha" class="form-label">Senha:</label>
           <input type="password" name="senha" id="senha" class="form-control">
         </div>
+        <?php if(isset($erro)):?>
+          <p class="alert alert-danger text-center p-2"><?= $erro ?></p>
+        <?php endif;?>
         <div class="mb-3 text-center">
           <button type="submit" class="btn btn-primary">Login</button>
         </div>
